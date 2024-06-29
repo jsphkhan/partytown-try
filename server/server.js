a557e9b641bc9f958b5a3f12aab2959f118c7fe0
Joseph Khan <jsphkhan@gmail.com>
2024-06-29 16:08:21 +0400

    writing custom seerver

---
+++ server/server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const next = require('next');
+const bodyParser = require('body-parser');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.disable('x-powered-by');
+
+    // health check api's
+    server.get('/ping', (req, res) => {
+      res.status(200).send('pong');
+    });
+    server.get('/health', (req, res) => {
+      res.status(200).send('success');
+    });
+
+    // parse application/json
+    server.use(bodyParser.json());
+
+    // routes
+    server.all('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    // server start
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit();
+  });
